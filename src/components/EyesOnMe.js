415dde1cd23c5ed1fd65800a623646965df64d84
Harris Sultan <harris_sultan@outlook.com>
2022-02-02 23:15:03 -0500

    commit tree 7278df8e4caeb5ce2298e7d832f9eeb70c0f5ab1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+
+function Focus() {
+    console.log("Good!");
+}
+
+function Blur() {
+    console.log("Hey! Eyes on me!");
+}
+
+function EyesOnMe(){
+    return(
+
+        <button value="Eyes on me" onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
+
+
+function handler(){
+    console.log('Entering password...')
+}
+function Keypad(){
+    return(
+        <input type="password" onChange={handler}/>
+    )  
+}
+
+
+
+export default Keypad;
